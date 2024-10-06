338c9fdd1b7332ad692c539f9d8dd2f2f227e9c4
etherealheim <128578761+etherealheim@users.noreply.github.com>
2024-10-06 17:30:45 +0200

    Init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,19 @@
-import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+import type { Config } from "tailwindcss";
 
-export default {
-  content: ["./src/**/*.tsx"],
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+export default config;
