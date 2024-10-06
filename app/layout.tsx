403124df175dbe29dcee43d71d639aaf04c25487
etherealheim <128578761+etherealheim@users.noreply.github.com>
2024-10-06 19:51:35 +0200

    Init3.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,16 +15,6 @@ import {
 
 import { ThemeProvider } from "@/components/theme-provider"
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 const satoshiSans = localFont({
   src: "./fonts/Satoshi-Variable.woff",
   variable: "--font-satoshi-sans",
