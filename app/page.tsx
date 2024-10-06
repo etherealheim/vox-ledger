1501c4cf14fea0a744ccf29c5956e198b3b30220
etherealheim <128578761+etherealheim@users.noreply.github.com>
2024-10-06 20:05:58 +0200

    Init4

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import HelloAnimation from "@/components/custom/hello-animation";
+
 export default function Home() {
   return (
     <div>
-
-      <section className="h-screen container mx-auto grid grid-cols-12 align-middle">
-        <h1 className="text-2xl font-normal col-span-12 flex items-center justify-center h-full text-center">Hello World. <br></br> It&apos;s time to wake up.</h1>
+      <section>
+        <HelloAnimation />
       </section>
     </div >
   );
---
+++ components/custom/hello-animation.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+const HelloAnimation = (): JSX.Element => {
+    const texts: string[] = [
+        "Hello World.",
+        "It's time to wake up.",
+        "It's time to make them accountable.",
+        "It's time for Vox Observer"
+
+    ];
+    const [currentTextIndex, setCurrentTextIndex] = useState<number>(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentTextIndex((prevIndex) => (prevIndex + 1) % texts.length);
+        }, 3000); // Change text every 4.5 seconds to allow full transition
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <div className="h-screen flex items-center justify-center">
+            <section className="container mx-auto">
+                <AnimatePresence mode="wait">
+                    <motion.h1
+                        key={currentTextIndex}
+                        className="text-2xl font-normal text-center"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ duration: 1.5 }}
+                    >
+                        {texts[currentTextIndex]}
+                    </motion.h1>
+                </AnimatePresence>
+            </section>
+        </div>
+    );
+};
+
+export default HelloAnimation;
